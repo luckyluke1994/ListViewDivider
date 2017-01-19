d0dda47731c562f12abebfcdd056de97b2c9eac3
luckyluke1994 <mddien1994@gmail.com>
2017-01-19 13:52:01 +0700

    first commit

---
+++ app/src/main/java/com/example/maidaidien/listviewdivider/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.maidaidien.listviewdivider;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[] data = {
+                "Mon 6/23 - Sunny - 31/17",
+                "Tue 6/24 - Foggy - 21/8",
+                "Wed 6/25 - Cloudy - 22/17",
+                "Thurs 6/26 - Rainy - 18/11",
+                "Fri 6/27 - Foggy - 21/10",
+                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                "Sun 6/29 - Sunny - 20/7"
+        };
+
+        List<String> list = new ArrayList<>(Arrays.asList(data));
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_row, R.id.title, list);
+        ListView listView = (ListView) findViewById(R.id.listview);
+        listView.setAdapter(adapter);
+    }
+}
